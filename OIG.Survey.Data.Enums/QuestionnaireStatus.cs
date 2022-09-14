4eccb35195035a958eb349077e4735333db5b4f6
Armand Jordaan <armandjordaan@live.co.za>
2022-09-14 12:32:04 +0200

    Add QueswtionnaireStatusEnum

---
+++ OIG.Survey.Data.Enums/QuestionnaireStatus.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace OIG.Survey.Data.Enums;
+
+public enum QuestionnaireStatus
+{
+    [Description("Concept")]
+    Concept = 0,
+    
+    [Description("Scheduled")]
+    Scheduled = 1,
+    
+    [Description("Active")]
+    Active = 2,
+    
+    [Description("Finished")]
+    Finished = 3
+}
